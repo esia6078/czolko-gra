a8fba5bd6c283563811eca892f6fdd8204679d4e
Dawid <dawid@gmail.com>
2026-06-22 00:25:12 +0200

    fix haslo

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const { Server } = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server);
+const io = new Server(server);git rm -r --cached .
 
 app.use(express.static('public'));
 
